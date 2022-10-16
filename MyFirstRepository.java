2318236f49d876140b16d392ad3a8df8c50d333c
Alex22835 <opashkevych@gmail.com>
2022-10-16 16:25:08 +0200

    Make a cycle "for"

--- MyFirstRepository.java
+++ MyFirstRepository.java
@@ -3,5 +3,9 @@ import java.sql.SQLOutput;
 public class MyFirstRepository {
     public static void main(String[] args) {
         System.out.println("My First Repository");
+
+        for(int a = 0;a < 10;a++){
+            System.out.println(a);
+        }
     }
 }
